09b09cfeb40dbf8ac9d3fff99783288fafd1973d
Axel Iradukunda <axel.iradukunda@student.ehb.be>
2018-04-20 21:32:14 +0200

    Personen aangepast/ Tableview geconfigureed

--- Axel_Iradukunda_S2IT_werkstuk1/Persoon.swift
+++ Axel_Iradukunda_S2IT_werkstuk1/Persoon.swift
@@ -5,23 +5,24 @@
 //  Created by student on 20/04/18.
 //  Copyright © 2018 student. All rights reserved.
 //
-
 import Foundation
+import UIKit
 
-class Persoon{
+class Persoon:NSObject{
     var naam:String
     var voornaam:String
-    var foto:String
+    var foto:UIImage
     var adres:String
     var gpscoordinaten:String
     var telefoonnummer:String
     
-    init() {
-        naam=""
-        voornaam=""
-        foto=""
-        adres=""
-        gpscoordinaten=""
-        telefoonnummer=""
+
+    init(naam:String,voornaam:String,foto:UIImage,adres:String,gpscoordinaten:String,telefoonnummer:String) {
+        self.naam=naam
+        self.voornaam=voornaam
+        self.foto=foto
+        self.adres=adres
+        self.gpscoordinaten=gpscoordinaten
+        self.telefoonnummer=telefoonnummer
     }
 }
