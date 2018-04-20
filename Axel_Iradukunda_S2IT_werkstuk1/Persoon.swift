a5c0901138ce7b56d8f86896b67d6b158c1a91e2
Axel Iradukunda <axel.iradukunda@student.ehb.be>
2018-04-20 19:27:22 +0200

    create an entity

---
+++ Axel_Iradukunda_S2IT_werkstuk1/Persoon.swift
@@ -0,0 +1,27 @@
+//
+//  Persoon.swift
+//  Axel_Iradukunda_S2IT_werkstuk1
+//
+//  Created by student on 20/04/18.
+//  Copyright © 2018 student. All rights reserved.
+//
+
+import Foundation
+
+class Persoon{
+    var naam:String
+    var voornaam:String
+    var foto:String
+    var adres:String
+    var gpscoordinaten:String
+    var telefoonnummer:String
+    
+    init() {
+        naam=""
+        voornaam=""
+        foto=""
+        adres=""
+        gpscoordinaten=""
+        telefoonnummer=""
+    }
+}
